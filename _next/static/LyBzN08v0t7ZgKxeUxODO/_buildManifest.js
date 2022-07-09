37846957b036c3436cd4fdcc04c4725dafbed919
anji0302 <32148732+anji0302@users.noreply.github.com>
2022-07-09 00:23:03 +0000

    Deploying to gh-pages from @ anji0302/my-personal@7ba1de98d129b4b4b75991cf24ebed1ff38af198 🚀

--- _next/static/LyBzN08v0t7ZgKxeUxODO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/46c9f256354d0e47.css","static/chunks/pages/index-519a651fbb9ac27b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
